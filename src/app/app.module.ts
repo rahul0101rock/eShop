9fa32cc52e11debf8adcf3ba97dc804167c8d22b
Rahul Choudhary <rahul0101rock@gmail.com>
2022-02-25 15:28:08 +0530

    Building product structure and product model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductItemComponent } from './products/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/products/product.model.ts
@@ -0,0 +1,16 @@
+export class Product{
+    public name: string;
+    public description:string;
+    public imagePath:string;
+    public price: number;
+    public category: string[];
+
+    constructor(name: string, description: string, imagePath: string, price: number, category: string[]){
+        this.name=name;
+        this.description=description;
+        this.imagePath=imagePath;
+        this.price=price;
+        this.category=category;
+    }
+
+}
