732db35ab242c5a0396bbb2bcf710be167b67b2a
Rahul Choudhary <rahul0101rock@gmail.com>
2022-03-01 18:07:13 +0530

    Changes in product detail and product list view

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -9,14 +9,25 @@ import { Cart } from '../cart/cart.model';
 export class ProductService {
 
   private products : Product[] = [
-    new Product("iPhone 12",
-    "A super-powerful chip. An advanced dual‑camera system. A Ceramic Shield front that’s tougher than any smartphone glass. And a bright, beautiful OLED display. iPhone 12 has it all — in two great sizes.",
-    "https://rukminim1.flixcart.com/image/416/416/kg8avm80/mobile/r/h/z/apple-iphone-12-dummyapplefsn-original-imafwg8duby8qbn4.jpeg?q=70",
-    59900.00,["Mobiles","Electronics","iPhone"]),
-     new Product("Boat Rockerz 510 Bluetooth Headphones",
+    new Product("Boat Rockerz 510 Bluetooth Headphones",
     "Rockerz 510 promises a battery back up of upto 20 hours and a standyby time of 250 hours. The foam inside the ear pads adapts to your ears with just the right pressure ensuring great comfort and also delivering authentic sound.It comes with dual connectivity, wireless via its bluetooth and wired with its aux port.",
     "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRsLQP3fgQVPTUkuwvkEcCNt8Y_ktED3cwOws_O9RmNrpGuEr6mvQVZbFOO3Rk0taef-h2_nW8p8thkvbYVTEbnKawRJyxM0xdbX9u6AbYGvMoaye9L_g3FFw&usqp=CAY",
-    1499.00,["Headphones","Electronics","Boat"])
+    1499.00,["Headphones","Electronics","Boat","WireLess"]),
+    new Product("Boat Rockerz 255 Neckband sport high bass headphone",
+    "While being lightweight in design pumps out your favorite tunes with powerful HD sound and deep boosted bass. The 10mm drivers installed to ensure that you get an exceptional sound experience with articulate audio reproduction with a blend of pitch-perfect frequency response. The sound produced by the boAt Rockerz 255 ensures that you differentiate music from noise and hear your favorite artists the way they were meant to be heard.",
+    "https://cf.shopee.in/file/bde4af11c0f54e3db8174be9b9717d9f",
+    1299,["Neckband","Electronics","Boat","WireLess"]),
+    new Product("Philips Multigroom series 3000",
+    "Includes a steel trimmer, a steel precision trimmer, a nose and ear hair trimmer, 3 hair trimming guards, 3 beard trimming guards, a stubble guard, an accessory storage bag, and a cleaning brush.",
+    "https://images.philips.com/is/image/PhilipsConsumer/MG3730_15-IMS-en_IN?wid=420&hei=360&$jpglarge$",
+    3931,["Trimmer","Electronics","Philips"]
+    ),
+    new Product("Logitech M331 Silent Plus Wireless Optical Mouse, Black",
+    "M331 is a result of Logitech's expertise and innovative approach designing comfortable solutions for over 25 years. Its asymmetric shape is meticulously crafted to guide your right hand to a natural position, while the soft rubber surface with distinctive patterns improves touch feeling. All that allows you to work or study comfortably for hours.",
+    "https://www.reliancedigital.in/medias/Logitech-M331-Silent-Plus-Wireless-Optical-Mouse-Black-491315724-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wyMjM1NDh8aW1hZ2UvanBlZ3xpbWFnZXMvaGRmL2hmNC84OTMwODIxMDEzNTM0LmpwZ3w0ZTZiMGJiNzNmMDg2ZDM3Mjg1NGY3MDM5NzMxZDNhYTBmZTViY2Q5YWQ0MDVhZjM0MWViMjg4Y2U0OGFiNzQ1",
+    1099,["Mouse","Electronics","Logitech","WireLess"])
+   
+     
   ];
 
   getProducts(){
