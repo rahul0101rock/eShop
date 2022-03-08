a740375eaf4235b08011c9d30cd925242051f9f0
Rahul Choudhary <rahul0101rock@gmail.com>
2022-03-08 17:10:12 +0530

    Created order model and order service

---
+++ src/app/order/order.model.ts
@@ -0,0 +1,14 @@
+import { Cart } from './../cart/cart.model';
+export class Order{
+    public orderItems: Cart[];
+    public dateTime: Date;
+    public address: string;
+    public orderState: string;
+
+    constructor(orderItems: Cart[],address: string){
+        this.orderItems = orderItems;
+        this.dateTime = new Date();
+        this.address = address;
+        this.orderState = "Ordered";
+    }
+}
---
+++ src/app/order/order.service.ts
@@ -0,0 +1,19 @@
+import { Order } from './order.model';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  private orders: Order[] = [];
+
+  constructor() { }
+
+  getOrders(){
+    return this.orders;
+  }
+  getOrder(index: number){
+    return this.orders[index];
+  }
+}
